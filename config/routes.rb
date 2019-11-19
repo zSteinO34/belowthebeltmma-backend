2f591b35a76d0e3a9946c83c34f15b38fee3198e
zSteinO34 <zstein1234@gmail.com>
2019-11-19 11:50:02 -0600

    added migrations, models, controllers for each table

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :tags
+  resources :post_tags
+  resources :comments
+  resources :likes
+  resources :posts
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
